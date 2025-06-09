79b222f2007ee868631f72c1e49c7be354a01ad5
guilherme-rms-cv <guilherme.rms.cv@gmail.com>
2025-06-09 14:39:42 -0300

    feat: incluir tratamento de string

--- src/main/java/br/ufma/ecp/jlox/Scanner.java
+++ src/main/java/br/ufma/ecp/jlox/Scanner.java
@@ -69,21 +69,42 @@ class Scanner {
                 addToken(SLASH);
               }
               break;
-              
-      case ' ':
-      case '\r':
-      case '\t':
-        // Ignore whitespace.
-        break;
 
-      case '\n':
-        line++;
-        break;
+            case ' ':
+            case '\r':
+            case '\t':
+              // Ignore whitespace.
+              break;
 
-      default:
-        Lox.error(line, "Unexpected character.");
-        break;
+            case '\n':
+              line++;
+              break;
+
+            case '"': string(); break;
+
+            default:
+              Lox.error(line, "Unexpected character.");
+              break;
+          }
+        }
+
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') line++;
+      advance();
     }
+
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    // The closing ".
+    advance();
+
+    // Trim the surrounding quotes.
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
   }
 
   private char peek() {
