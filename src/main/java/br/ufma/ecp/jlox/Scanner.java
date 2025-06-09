123db20ef6f3b027151d2b4c09c5e40ce2a7096c
guilherme-rms-cv <guilherme.rms.cv@gmail.com>
2025-06-09 15:04:33 -0300

    feat: inclui identificador e palavra reservada

--- src/main/java/br/ufma/ecp/jlox/Scanner.java
+++ src/main/java/br/ufma/ecp/jlox/Scanner.java
@@ -15,6 +15,28 @@ class Scanner {
   private int current = 0;
   private int line = 1;
 
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
+
   Scanner(String source) {
     this.source = source;
   }
@@ -83,10 +105,52 @@ class Scanner {
             case '"': string(); break;
 
             default:
-              Lox.error(line, "Unexpected character.");
+              if (isDigit(c)) {
+                number();
+              } else if (isAlpha(c)) {
+                identifier();
+              }
+              else {
+                Lox.error(line, "Unexpected character.");
+              }
               break;
           }
         }
+
+ private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+
+    addToken(IDENTIFIER);
+  }
+
+ private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  } 
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+
+      while (isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER,
+        Double.parseDouble(source.substring(start, current)));
+  }
 
   private void string() {
     while (peek() != '"' && !isAtEnd()) {
@@ -105,6 +169,11 @@ class Scanner {
     // Trim the surrounding quotes.
     String value = source.substring(start + 1, current - 1);
     addToken(STRING, value);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
   }
 
   private char peek() {
