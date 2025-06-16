f4285c9c5e5fe6681a0480d2134ae3bb6fe6d0f2
guilherme-rms-cv <guilherme.rms.cv@gmail.com>
2025-06-16 17:36:49 -0300

    feat: implementa analisador léxico (Scanner)

--- src/main/java/br/ufma/ecp/jlox/Scanner.java
+++ src/main/java/br/ufma/ecp/jlox/Scanner.java
@@ -120,7 +120,10 @@ class Scanner {
  private void identifier() {
     while (isAlphaNumeric(peek())) advance();
 
-    addToken(IDENTIFIER);
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+      addToken(type);
   }
 
  private boolean isAlpha(char c) {
