831d4bb3e40006f1f93d0a99d09cee06542bd40a
guilherme-rms-cv <guilherme.rms.cv@gmail.com>
2025-07-13 19:32:20 -0300

    feat(interpreter): implementa suporte a variáveis globais e avaliação de identificadores

--- src/main/java/br/ufma/ecp/jlox/Interpreter.java
+++ src/main/java/br/ufma/ecp/jlox/Interpreter.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>,
                              Stmt.Visitor<Void> {
+    private Environment environment = new Environment();
+
     void interpret(List<Stmt> statements) {
         try {
             for (Stmt statement : statements) {
@@ -32,6 +34,17 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+
+    environment.define(stmt.name.lexeme, value);
+    return null;
+    }
+
+    @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
     }
@@ -54,6 +67,11 @@ class Interpreter implements Expr.Visitor<Object>,
         }
 
         return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
     }
 
     @Override
