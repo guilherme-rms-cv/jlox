c31e10c73042cf277f2756e60e88fc62ec2de2ef
guilherme-rms-cv <guilherme.rms.cv@gmail.com>
2025-07-13 21:46:45 -0300

    feat: implement while loops with condition evaluation and execution

--- src/main/java/br/ufma/ecp/tool/GenerateAst.java
+++ src/main/java/br/ufma/ecp/tool/GenerateAst.java
@@ -29,6 +29,8 @@ public class GenerateAst {
       "If         : Expr condition, Stmt thenBranch," +
                   " Stmt elseBranch",
       "Print      : Expr expression",
+      "Var        : Token name, Expr initializer",
+      "While      : Expr condition, Stmt body",
       "Var        : Token name, Expr initializer"
     ));
   }
