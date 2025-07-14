5353d7b7455600829642ad7ec14c25f4d8168d41
guilherme-rms-cv <guilherme.rms.cv@gmail.com>
2025-07-13 21:34:45 -0300

    feat: implement if statement evaluation in interpreter

--- src/main/java/br/ufma/ecp/tool/GenerateAst.java
+++ src/main/java/br/ufma/ecp/tool/GenerateAst.java
@@ -14,6 +14,7 @@ public class GenerateAst {
     String outputDir = args[0];
 
     defineAst(outputDir, "Expr", Arrays.asList(
+      "Assign   : Token name, Expr value",
       "Binary   : Expr left, Token operator, Expr right",
       "Grouping : Expr expression",
       "Literal  : Object value",
@@ -22,7 +23,10 @@ public class GenerateAst {
     ));
     
     defineAst(outputDir, "Stmt", Arrays.asList(
+      "Block      : List<Stmt> statements",
       "Expression : Expr expression",
+      "If         : Expr condition, Stmt thenBranch," +
+                  " Stmt elseBranch",
       "Print      : Expr expression",
       "Var        : Token name, Expr initializer"
     ));
