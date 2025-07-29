31e9f36bd7758ed0609d1843f4971e3d54fcac3f
guilherme-rms-cv <guilherme.rms.cv@gmail.com>
2025-07-28 23:11:07 -0300

    feat: define AST e impressor de expressões (AstPrinter)

---
+++ src/main/java/br/ufma/ecp/AstPrinter.java
@@ -0,0 +1,97 @@
+package br.ufma.ecp;
+
+class AstPrinter implements Expr.Visitor<String> {
+  String print(Expr expr) {
+    return expr.accept(this);
+  }
+
+  @Override
+  public String visitBinaryExpr(Expr.Binary expr) {
+    return parenthesize(expr.operator.lexeme,
+                        expr.left, expr.right);
+  }
+
+  @Override
+  public String visitAssignExpr(Expr.Assign expr) {
+      return parenthesize("assign " + expr.name.lexeme, expr.value);
+  }
+
+  @Override
+  public String visitCallExpr(Expr.Call expr) {
+      StringBuilder builder = new StringBuilder();
+      builder.append("(call ");
+      builder.append(expr.callee.accept(this));
+      for (Expr argument : expr.arguments) {
+          builder.append(" ");
+          builder.append(argument.accept(this));
+      }
+      builder.append(")");
+      return builder.toString();
+  }
+
+  @Override
+  public String visitLogicalExpr(Expr.Logical expr) {
+    return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
+
+  @Override
+  public String visitGetExpr(Expr.Get expr) {
+      return "(get " + expr.object.accept(this) + "." + expr.name.lexeme + ")";
+  }
+
+  @Override
+  public String visitVariableExpr(Expr.Variable expr) {
+    return expr.name.lexeme;
+  }
+
+  @Override
+  public String visitThisExpr(Expr.This expr) {
+      return "this";
+  }
+
+  @Override
+  public String visitSetExpr(Expr.Set expr) {
+      return expr.object.accept(this) + "." + expr.name.lexeme + " = " + expr.value.accept(this);
+  }
+
+  @Override
+  public String visitGroupingExpr(Expr.Grouping expr) {
+    return parenthesize("group", expr.expression);
+  }
+
+  @Override
+  public String visitLiteralExpr(Expr.Literal expr) {
+    if (expr.value == null) return "nil";
+    return expr.value.toString();
+  }
+
+  @Override
+  public String visitUnaryExpr(Expr.Unary expr) {
+    return parenthesize(expr.operator.lexeme, expr.right);
+  }
+
+  private String parenthesize(String name, Expr... exprs) {
+    StringBuilder builder = new StringBuilder();
+
+    builder.append("(").append(name);
+    for (Expr expr : exprs) {
+      builder.append(" ");
+      builder.append(expr.accept(this));
+    }
+    builder.append(")");
+
+    return builder.toString();
+  }
+
+  public static void main(String[] args) {
+    Expr expression = new Expr.Binary(
+        new Expr.Unary(
+            new Token(TokenType.MINUS, "-", null, 1),
+            new Expr.Literal(123)),
+        new Token(TokenType.STAR, "*", null, 1),
+        new Expr.Grouping(
+            new Expr.Literal(45.67)));
+
+    System.out.println(new AstPrinter().print(expression));
+  }
+}
