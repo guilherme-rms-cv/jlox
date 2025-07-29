96aea8101816a645de014760eba38f26664fb115
guilherme-rms-cv <guilherme.rms.cv@gmail.com>
2025-07-28 23:11:40 -0300

    feat: adiciona suporte a funções e chamadas

---
+++ src/main/java/br/ufma/ecp/LoxCallable.java
@@ -0,0 +1,8 @@
+package br.ufma.ecp;
+
+import java.util.List;
+
+interface LoxCallable {
+  int arity();
+  Object call(Interpreter interpreter, List<Object> arguments);
+}
---
+++ src/main/java/br/ufma/ecp/Return.java
@@ -0,0 +1,10 @@
+package br.ufma.ecp;
+
+class Return extends RuntimeException {
+  final Object value;
+
+  Return(Object value) {
+    super(null, null, false, false);
+    this.value = value;
+  }
+}
